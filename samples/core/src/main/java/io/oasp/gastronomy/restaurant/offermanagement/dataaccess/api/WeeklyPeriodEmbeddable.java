4ec0fcd42f2bb2f77b0a94a999e0fd4beff6fa35
Krzysztof Sobkowiak <krzysztof.sobkowiak@capgemini.com>
2019-01-17 10:27:14 +0100

    Special entity corrected

--- samples/core/src/main/java/io/oasp/gastronomy/restaurant/offermanagement/dataaccess/api/WeeklyPeriodEmbeddable.java
+++ samples/core/src/main/java/io/oasp/gastronomy/restaurant/offermanagement/dataaccess/api/WeeklyPeriodEmbeddable.java
@@ -1,54 +1,68 @@
 package io.oasp.gastronomy.restaurant.offermanagement.dataaccess.api;
 
+import java.time.DayOfWeek;
+
 import javax.persistence.Embeddable;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
-import java.time.DayOfWeek;
+
+import io.oasp.gastronomy.restaurant.offermanagement.common.api.WeeklyPeriod;
 
 @Embeddable
-public class WeeklyPeriodEmbeddable {
+public class WeeklyPeriodEmbeddable implements WeeklyPeriod {
 
-	private DayOfWeek startingDay;
+  private DayOfWeek startingDay;
 
-	private int startingHour;
+  private int startingHour;
 
-	private DayOfWeek endingDay;
+  private DayOfWeek endingDay;
 
-	private int endingHour;
+  private int endingHour;
 
-	public DayOfWeek getStartingDay() {
-		return startingDay;
-	}
+  private static final long serialVersionUID = 1L;
 
-	public void setStartingDay(DayOfWeek startingDay) {
-		this.startingDay = startingDay;
-	}
+  public DayOfWeek getStartingDay() {
 
-	@Max(24)
-	@Min(0)
-	public int getStartingHour() {
-		return startingHour;
-	}
+    return startingDay;
+  }
 
-	public void setStartingHour(int startingHour) {
-		this.startingHour = startingHour;
-	}
+  public void setStartingDay(DayOfWeek startingDay) {
 
-	public DayOfWeek getEndingDay() {
-		return endingDay;
-	}
+    this.startingDay = startingDay;
+  }
 
-	public void setEndingDay(DayOfWeek endingDay) {
-		this.endingDay = endingDay;
-	}
+  @Max(24)
+  @Min(0)
+  public int getStartingHour() {
 
-	@Max(24)
-	@Min(0)
-	public int getEndingHour() {
-		return endingHour;
-	}
+    return startingHour;
+  }
 
-	public void setEndingHour(int endingHour) {
-		this.endingHour = endingHour;
-	}
+  public void setStartingHour(int startingHour) {
+
+    this.startingHour = startingHour;
+  }
+
+  public DayOfWeek getEndingDay() {
+
+    return endingDay;
+  }
+
+  public void setEndingDay(DayOfWeek endingDay) {
+
+    this.endingDay = endingDay;
+  }
+
+  @Max(24)
+  @Min(0)
+  public int getEndingHour() {
+
+    return endingHour;
+  }
+
+  public void setEndingHour(int endingHour) {
+
+    this.endingHour = endingHour;
+  }
+
 }
