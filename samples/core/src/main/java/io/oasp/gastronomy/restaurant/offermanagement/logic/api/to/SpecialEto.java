cb160c319bb44280906427329d4de45beb65cc4f
Krzysztof Sobkowiak <krzysztof.sobkowiak@capgemini.com>
2019-01-17 10:40:32 +0100

    Special entity corrected

--- samples/core/src/main/java/io/oasp/gastronomy/restaurant/offermanagement/logic/api/to/SpecialEto.java
+++ samples/core/src/main/java/io/oasp/gastronomy/restaurant/offermanagement/logic/api/to/SpecialEto.java
@@ -2,50 +2,69 @@ package io.oasp.gastronomy.restaurant.offermanagement.logic.api.to;
 
 import io.oasp.gastronomy.restaurant.general.common.api.datatype.Money;
 import io.oasp.gastronomy.restaurant.offermanagement.common.api.Special;
+import io.oasp.gastronomy.restaurant.offermanagement.common.api.WeeklyPeriod;
 import io.oasp.module.basic.common.api.to.AbstractEto;
 
 public class SpecialEto extends AbstractEto implements Special {
 
-	private static final long serialVersionUID = -4457788068370871480L;
+  private static final long serialVersionUID = -4457788068370871480L;
 
-	private String name;
+  private String name;
 
-	private OfferEto offer;
+  private OfferEto offer;
 
-	private WeeklyPeriod activePeriod;
+  private WeeklyPeriod activePeriod;
 
-	private Money specialPrice;
+  private Money specialPrice;
 
-	public String getName() {
+  private Long offerId;
+
+  public String getName() {
 		return name;
-	}
+  }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+  public void setName(String name) {
 
-	public OfferEto getOffer() {
+    this.name = name;
+  }
+
+  public OfferEto getOffer() {
 		return offer;
-	}
+  }
 
-	public void setOffer(OfferEto offer) {
-		this.offer = offer;
-	}
+  public void setOffer(OfferEto offer) {
 
-	public WeeklyPeriod getActivePeriod() {
+    this.offer = offer;
+  }
+
+  public WeeklyPeriod getActivePeriod() {
 		return activePeriod;
-	}
+  }
 
-	public void setActivePeriod(WeeklyPeriod activePeriod) {
-		this.activePeriod = activePeriod;
-	}
+  public void setActivePeriod(WeeklyPeriod activePeriod) {
 
-	public Money getSpecialPrice() {
+    this.activePeriod = activePeriod;
+  }
+
+  public Money getSpecialPrice() {
 		return specialPrice;
-	}
+  }
 
-	public void setSpecialPrice(Money specialPrice) {
-		this.specialPrice = specialPrice;
-	}
+  public void setSpecialPrice(Money specialPrice) {
+
+    this.specialPrice = specialPrice;
+  }
+
+  @Override
+  public Long getOfferId() {
+
+    return offerId;
+  }
+
+  @Override
+  public void setOfferId(Long offerId) {
+
+    this.offerId = offerId;
+  }
 
 }
