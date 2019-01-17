f32ed5c0e6a91f0fbcade4d55fb3992d14ab6043
Krzysztof Sobkowiak <krzysztof.sobkowiak@capgemini.com>
2019-01-17 10:27:14 +0100

    Special entity corrected

--- samples/core/src/main/java/io/oasp/gastronomy/restaurant/offermanagement/dataaccess/api/SpecialEntity.java
+++ samples/core/src/main/java/io/oasp/gastronomy/restaurant/offermanagement/dataaccess/api/SpecialEntity.java
@@ -1,10 +1,18 @@
 package io.oasp.gastronomy.restaurant.offermanagement.dataaccess.api;
 
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
 import io.oasp.gastronomy.restaurant.general.common.api.datatype.Money;
 import io.oasp.gastronomy.restaurant.general.dataaccess.api.ApplicationPersistenceEntity;
 import io.oasp.gastronomy.restaurant.offermanagement.common.api.Special;
-
-import javax.persistence.*;
+import io.oasp.gastronomy.restaurant.offermanagement.common.api.WeeklyPeriod;
 
 @Entity
 @Table(name = "Special")
@@ -18,40 +26,78 @@ public class SpecialEntity extends ApplicationPersistenceEntity implements Speci
 
   private Money specialPrice;
 
+  private static final long serialVersionUID = 1L;
+
   @Column(unique = true)
   public String getName() {
-    return name;
+
+    return this.name;
   }
 
   public void setName(String name) {
+
     this.name = name;
   }
 
   @ManyToOne(fetch = FetchType.EAGER)
   @JoinColumn(name = "offerId")
   public OfferEntity getOffer() {
-    return offer;
+
+    return this.offer;
   }
 
   public void setOffer(OfferEntity offer) {
+
     this.offer = offer;
   }
 
   @Embedded
   public WeeklyPeriodEmbeddable getActivePeriod() {
-    return activePeriod;
+
+    return this.activePeriod;
   }
 
-  public void setActivePeriod(WeeklyPeriodEmbeddable activePeriod) {
-    this.activePeriod = activePeriod;
+  public void setActivePeriod(WeeklyPeriod activePeriod) {
+
+    WeeklyPeriodEmbeddable period = new WeeklyPeriodEmbeddable();
+    period.setEndingDay(activePeriod.getEndingDay());
+    period.setEndingHour(activePeriod.getEndingHour());
+    period.setStartingDay(activePeriod.getStartingDay());
+    period.setStartingHour(activePeriod.getStartingHour());
+
+    this.activePeriod = period;
   }
 
   public Money getSpecialPrice() {
-    return specialPrice;
+
+    return this.specialPrice;
   }
 
   public void setSpecialPrice(Money specialPrice) {
+
     this.specialPrice = specialPrice;
   }
-}
 
+  @Override
+  @Transient
+  public Long getOfferId() {
+
+    if (this.offer == null) {
+      return null;
+    }
+    return this.offer.getId();
+  }
+
+  @Override
+  public void setOfferId(Long offerId) {
+
+    if (offerId == null) {
+      this.offer = null;
+    } else {
+      OfferEntity offerEntity = new OfferEntity();
+      offerEntity.setId(offerId);
+      this.offer = offerEntity;
+    }
+  }
+
+}
